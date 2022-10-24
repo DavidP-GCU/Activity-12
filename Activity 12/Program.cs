e4c59c2c2622b4bf64e01f780248198f2384e4d6
DavidP-GCU <114124010+DavidP-GCU@users.noreply.github.com>
2022-10-23 19:17:36 -0600

    Add files via upload

---
+++ Activity 12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+public class HelloWorld
+{
+    public static void Main()
+    {
+        try //begin exception handling
+        {
+            // Create varibales. 
+            int i;
+            int word;
+            int l;
+
+
+            //Create string variable. 
+            string str = System.IO.File.ReadAllText("Input.txt");
+
+            l = 1;
+            word = 1;
+
+            // Create loop that will end once it reaches the end of the string
+            while (l <= str.Length - 1)
+            { 
+                if ((str[l] == '.' || str[l] == ' ' || str[l] == '\t' || str[l] == '!' || str[l] == ',' || str[l] == '?' || str[l] == '\n') && (str[l - 1] == 't' || str[l - 1] == 'e'))
+                {
+                    word++;
+                }
+
+                l++;
+            }
+            Console.Write("There are {0} words ending in t or e", word);
+        }
+        catch (Exception ex) //end exception handler
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+
+    }
+}
